fab5c23ecc4efcbeca44f01a8cc6ee119d3b7e06
Fabio Henrique Clem da Silva <fabio.silva3@gvt.com.br>
2018-04-17 15:35:27 -0300

    pre settings for assoc olt

--- html/associacao_olt/js/associacao_olt.js
+++ html/associacao_olt/js/associacao_olt.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
     var instancia;
     var listaSerial;
+
     // Habilita loading
     $("#loading").css("display", "block");
     // Disabilita formulario
     $("#formSelectOlt").css("display", "none");
+
+    // Desabilita mensagem
+    $("#mensagem").css("display", "none");
+
     if (window.location.href) {
         var link = window.location.href;
         var split = link.split("=");
@@ -21,7 +26,6 @@ $(document).ready(function () {
         request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     }
 
-
     request.send(JSON.stringify(paramObj));
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
@@ -29,7 +33,8 @@ $(document).ready(function () {
                 listaSerial = JSON.parse(request.responseText);
                 var select = document.getElementById("selectOlt");
                 for (var index = 0; index < listaSerial.length; index++) {
-                    select.options[select.options.length] = new Option(listaSerial[index].serial + " / Slot: " + listaSerial[index].slot + " - Porta: " + listaSerial[index].porta);
+                    select.options[select.options.length] =
+                        new Option(listaSerial[index].serial + " / Slot: " + listaSerial[index].slot + " - Porta: " + listaSerial[index].porta, listaSerial[index].serial);
                 }
                 // Desaabilita loading
                 $("#loading").css("display", "none");
@@ -40,13 +45,24 @@ $(document).ready(function () {
     };
 
     $("#assoc_id").click(function () {
-
-        // Faz o ajax passando as informações para associar...
-        // $("#formSelectOlt").css("display", "none");
-        // $("#loading").css("display", "block");
-
+        // Hide form
+        $("#formSelectOlt").css("display", "none");
+        // Loading On
+        $("#loading").css("display", "block");
+        // Get selected option from select
+        var selectedOlt = $("#selectOlt").val();
+        // Validação do request.
+        if (selectedOlt === "") {
+            alert("Por favor selecione uma das opções.");
+        } else {
+            // Request
+            // Change to request and modify the msg
+            setTimeout(function () {
+                $("#loading").css("display", "none"); // Loading Off
+                $("#mensagem").css("display", "block"); // Mensagem On
+                $("#textoMensagem").text("Selecionado o serial: " + selectedOlt + " & Instancia: " + instancia); // Mount msg in screen
+            }, 1000);
+        }
     });
-
-
 
 });
