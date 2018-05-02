ccd7f707de80b6d6a3dc6a200ebeb2a75ce3e5af
Fabio Henrique Clem da Silva <fabio.silva3@gvt.com.br>
2018-05-02 17:50:35 -0300

    Montado info de dispositivos conectados com mock *Koalization*

--- pages/dispcon/js/dispcon.js
+++ pages/dispcon/js/dispcon.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     // Variaveis do sistema
     var instancia;
 
+    var eqplist;
+
     getInstancia();
 
     function getInstancia() {
@@ -26,14 +28,94 @@ $(document).ready(function () {
     function mountCommand() {
 
         var _data = JSON.stringify({ "instancia": instancia, "parametro": null, "execucao": "GET_EQP_DISP_CON" });
-
-        console.log(_data);
-
-
-
+        eqplist = [
+            {
+                serial: "11111", guid: 1,
+                dispositivos: [
+                    {
+                        dispositivo: "ABC",
+                        status: "ativo"
+                    },
+                    {
+                        dispositivo: "BAC",
+                        status: "inativo"
+                    }
+                ]
+            },
+            {
+                serial: "22222", guid: 2
+                ,
+                dispositivos: [
+                    {
+                        dispositivo: "ABC",
+                        status: "inativo"
+                    }
+                ]
+            },
+            {
+                serial: "33333", guid: 3,
+                dispositivos: [
+                    {
+                        dispositivo: "ABC",
+                        status: "ativo"
+                    },
+                    {
+                        dispositivo: "BAC",
+                        status: "ativo"
+                    },
+                    {
+                        dispositivo: "CAB",
+                        status: "inativo"
+                    }
+                ]
+            }
+        ];
+        mounttable();
     }
 
+    function mounttable() {
+        setFormOption("none");
+        setLoadingOptions("block", "Aguarde...");
+        setTimeout(function () {
+            for (var index = 0; index < eqplist.length; index++) {
+                var eqp = eqplist[index];
+                $("#eqpListbody:last-child").append("<tr> <td> " + eqp.serial + " </td> <td> <button class='btn btn-blue btn-margin-bottom' type='buttton' id='view" + index + "' >Visualizar</button> </td> </tr>");
+            }
+            setLoadingOptions("none", null);
+            setFormOption("block");
+            setEqpListOption("block");
+            // setDispListOption("none");
+            mountRequest();
+        }, 1000);
+    }
 
+    function mountRequest() {
+        $("tr").each(function (index) {
+            $("#view" + index).click(function () {
+                $("#dispListbody").empty();
+                // setEqpListOption("none");
+                // setDispListOption("block");
+                mountTableListDisp(index);
+            });
+        });
+    }
+
+    function mountTableListDisp(i) {
+        var disp = eqplist[i].dispositivos;
+        // setDispListOption("block");
+        for (var index = 0; index < disp.length; index++) {
+            var dispE = disp[index];
+            $("#dispListbody:last-child").append("<tr> <td> " + dispE.dispositivo + " </td> <td> " + dispE.status + " </td> </tr>");
+        }
+    }
+
+    function setEqpListOption(show) {
+        $("#eqplist").css("display", show)
+    }
+
+    function setDispListOption(show) {
+        $("#displist").css("display", show)
+    }
 
     /**
     * Importar para todos os scripts e Manter o padrão \/
